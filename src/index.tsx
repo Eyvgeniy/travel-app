3831fe111dd9277180143bd1af77c1562f155bfe
Eyvgeniy <antonm33@yandex.ru>
2021-03-08 22:43:21 +0200

    refactor: remove assets to public

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,12 @@
-import * as React from "react";
+import React from "react";
 import { render } from "react-dom";
 import App from "./components/App";
+import routes from "./routes";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const rootEl = document.getElementById("root");
-
+fetch(routes.getCountries())
+  .then((res) => res.json())
+  .then(console.log)
+  .catch((err) => console.log(err.message));
 render(<App />, rootEl);
