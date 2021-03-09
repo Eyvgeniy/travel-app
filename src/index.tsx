d7661b3b48e880c58a62a7d04c665cd14864ed95
Eyvgeniy <antonm33@yandex.ru>
2021-03-09 09:00:01 +0200

    feat: add state and init state on load

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,34 @@
+import "regenerator-runtime/runtime";
 import React from "react";
 import { render } from "react-dom";
 import App from "./components/App";
 import routes from "./routes";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from "./slices";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const rootEl = document.getElementById("root");
-fetch(routes.getCountries())
-  .then((res) => res.json())
-  .then(console.log)
-  .catch((err) => console.log(err.message));
-render(<App />, rootEl);
+const init = async () => {
+  const rootEl = document.getElementById("root");
+
+  const f = async () => {
+    const res = await fetch(routes.getCountries());
+    const data = await res.json();
+
+    return await { countries: data };
+  };
+  const preloadedState = await f();
+
+  const store = configureStore({
+    reducer: rootReducer,
+    preloadedState,
+  });
+
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    rootEl,
+  );
+};
+init();
