43f2772285cd45b01e365a797308967d1830b22c
Dmitry M <mr.blacky01@gmail.com>
2021-03-14 23:15:40 +0100

    refactor: change loaders for scss and typescript support for them

--- configs/webpack/common.js
+++ configs/webpack/common.js
@@ -2,8 +2,10 @@
 const { resolve } = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CheckerPlugin } = require('awesome-typescript-loader')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const getCSSModuleLocalIdent = require('react-dev-utils/getCSSModuleLocalIdent');
 
-module.exports = {
+module.exports  = {
   resolve: {
     extensions: [".js", ".jsx", ".ts", ".tsx"],
   },
@@ -16,18 +18,65 @@ module.exports = {
         exclude: /node_modules/,
       },
       {
-        test: [/\.jsx?$/],
-        use: ["babel-loader"],
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env'],
+            },
+        }
+    },
+      {
+        test: /\.module\.s(a|c)ss$/,
+        exclude: /node_modules/,
+        use: [
+            'style-loader',
+            'css-modules-typescript-loader',
+            {
+                loader: 'css-loader',
+                options: {
+                    importLoaders: 1,
+                    modules: {
+                        getLocalIdent: getCSSModuleLocalIdent
+                    },
+                    sourceMap: true
+                }
+            },
+            {
+                loader: 'sass-loader',
+                options: {
+                    sourceMap: true
+                }
+            }
+        ]
       },
       {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(scss|sass)$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
+        test: /\.s(a|c)ss$/,
+        exclude: [
+            /\.module\.(s(a|c)ss)$/,
+            /node_modules/
+        ],
+        use: [
+            'style-loader',
+            {
+                loader: 'css-loader',
+                options: {
+                    sourceMap: true
+                }
+            },
+            {
+                loader: 'sass-loader',
+                options: {
+                    sourceMap: true
+                }
+            }
+        ]
+      },   
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
         use: [
@@ -40,6 +89,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({ template: "index.html.ejs" }),
     new CheckerPlugin(),
+    new MiniCssExtractPlugin(),
   ],
   externals: {
     react: "React",
@@ -48,4 +98,4 @@ module.exports = {
   performance: {
     hints: false,
   },
-};
+}
